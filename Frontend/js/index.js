568400f3f73019c3972e4ce93879cb1eb4085ba1
AdrGia <boulanger.Adrien.75@gmail.com>
2021-08-16 17:03:50 +0200

    rajout sur le js index

--- Frontend/js/index.js
+++ Frontend/js/index.js
@@ -4,3 +4,11 @@ fetch("http://localhost:3000/api/cameras")
 .then((response) => response.json())
 .then((cameras) => cameras)
 .catch((error) => { alert("Erreur de connexion avec le serveur")})
+
+const camerasDiv = document.getElementById("cameras");
+
+const camerasArticle = document.createElement("article");
+camerasDiv.apprendChild(camerasArticle);
+camerasArticle.className = "cameras";
+
+const productLink = document.creatElement("a");
