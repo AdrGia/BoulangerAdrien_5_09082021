9a83ed81bafe7d1869dbb122e608013961721dea
AdrGia <boulanger.Adrien.75@gmail.com>
2021-08-16 15:22:17 +0200

    modification du js

--- Frontend/js/index.js
+++ Frontend/js/index.js
@@ -1,16 +1,6 @@
 // Récupération des données de l'API //
 
 fetch("http://localhost:3000/api/cameras")
-    .then(function(reponse){
-      console.log("zero")
-      return reponse.json()
-    })
-
-    .then(function(cameras){
-      console.log(cameras)
-    })
-
-    .catch(function(error){
-      console.log("panier")
-      alert(error)
-    })
+.then((response) => response.json())
+.then((cameras) => cameras)
+.catch((error) => { alert("Erreur de connexion avec le serveur")})
