22c5538120336ec29f71c3668c24b6292932d61a
AdrGia <boulanger.Adrien.75@gmail.com>
2021-08-17 18:21:39 +0200

    rajout sur le js index

--- Frontend/js/index.js
+++ Frontend/js/index.js
@@ -1,14 +1,27 @@
 // Récupération des données de l'API //
 
+async function article() {
+  const cameras = await getCameras()
+
+  for (camera of cameras) {
+    diplayCameras(camera)
+  }
+}
+
 fetch("http://localhost:3000/api/cameras")
 .then((response) => response.json())
 .then((cameras) => cameras)
 .catch((error) => { alert("Erreur de connexion avec le serveur")})
 
-const camerasDiv = document.getElementById("cameras");
 
-const camerasArticle = document.createElement("article");
-camerasDiv.apprendChild(camerasArticle);
-camerasArticle.className = "cameras";
+function displayCameras(camera) {
+  const templateElt = document.getElementById("camera")
+  const cloneElt = document.importNode(templateElt.content, true)
 
-const productLink = document.creatElement("a");
+  cloneElt.getElementById("camerasLink")
+  cloneElt.getElementById("camerasImage").src = camera.imageUrl
+  cloneElt.getElementById("camerasName").textContent = camera.name
+  cloneElt.getElementById("camerasPrice").textContent = camera.price 
+  cloneElt.getElementById("camerasDescription").textContent = camera.description
+
+  document.getElementById("article").appendChild(cloneElt)
