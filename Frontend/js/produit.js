36ea1aa472eba42ab67a9d057dc80e33a3c37454
AdrGia <boulanger.Adrien.75@gmail.com>
2021-08-24 17:06:25 +0200

    modifications du html produit + js

--- Frontend/js/produit.js
+++ Frontend/js/produit.js
@@ -1,6 +1,9 @@
 
-let params = new URL(document.location).searchParams;
-let id = params.get("id");
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const id = urlParams.get('id');
+console.log(id);
+
 const getCameras = async function(){
 
 	try {
@@ -10,10 +13,37 @@ const getCameras = async function(){
     	if (response.ok) {
         let cameras = await response.json();
         console.log(camera);
-    	}	
-    }
+    	
+      const article = document.getElementById("page_product");
+
+    	const cameraDiv = document.createElement("div");
+    	page_product.appendChild(cameraDiv);
+    	cameraDiv.className = "camera_ref";
+
+    	const cameraImg = document.createElement("img");
+    	page_product.appendChild(cameraImg);
+    	cameraImg.setAttribute("src", camera.imageUrl);
+    	cameraImg.setAttribute("alt", "Caméra vintage" + camera.name);
+    	cameraImg.setAttribute("title", "Caméra vintage" + camera.name);
+
+    	const cameraDivInfo = document.createElement("div");
+    	page_product.appendChild(cameraDivInfo);
+    	cameraDivInfo.className = camera_info;
+
+      const cameraH4 = document.createElement("h4");
+      page_product.appendChild(cameraH4);
+      cameraH4.className = "camera_h4";
+
+      const cameraP = document.createElement("p");
+      page_product.appendChild(cameraP);
+      cameraP.textContent = "camera.name";
+    }  
+
+  }
     catch (error) {
     	alert("Erreur :" + error);
     }
 	     
-}    	
+}
+
+getCameras();    	
